fc3854ecd649c4a6ec912ecee54b689392b098e5
Brian Liotti <bdliotti@gmail.com>
2018-07-12 14:47:13 -0400

    Added Menu

--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -1,3 +1,5 @@
+import React from "react"
+import { Link } from "react-router-dom"
 /*  
   TODO: 
   
@@ -13,12 +15,26 @@
    send the user to the page that displays a list of colors
 */
 
-
 const Menu = props => {
-    return (
-      <div>
-        <h1>Five in One</h1>
-        
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>Five in One</h1>
+      <ul>
+        <li>
+          <Link to="/colors">Colors</Link>
+        </li>
+        <li>
+          <Link to="/buzzwords">Buzz Words</Link>
+        </li>
+        <li>
+          <Link to="/starwars">Star Wars Names</Link>
+        </li>
+        <li>
+          <Link to="/cookie">Fotune Cookie</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Menu
